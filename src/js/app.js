92014aa02ee8df1cb146d7c2ba5d8b6f5659fff2
jc_adavydenko <zabell2814@gmail.com>
2017-12-29 00:03:12 +0200

    fixed vh section on iphone

--- src/js/app.js
+++ src/js/app.js
@@ -12,8 +12,23 @@ import common from './modules/common';
   $(() => {
     new WOW().init();
     var scroll = new SmoothScroll('a[href*="#"]');
+  /* Fix iOS 7 bug for -unit support */
 
-     
+  function fixScroll() {
+
+    var iOS = navigator.userAgent.match(/(iPod|iPhone|iPad|Android)/);
+    
+    if (iOS) {
+        $(window).bind('load orientationchange', iosVhHeightBug);
+    }
+    function iosVhHeightBug() {
+        setTimeout(function () {
+            var windowH = $(window).height();
+            $("#header").css('height', windowH);
+        }, 300); 
+    }
+  }
+  fixScroll();
 
   });
 
